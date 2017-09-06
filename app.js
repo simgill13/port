9793f228a3212a2819eb6458817140b379fe3604
Your Name <sim@Sims-MacBook-Pro.local>
2017-09-06 13:31:35 -0700

    test

--- app.js
+++ app.js
@@ -290,4 +290,13 @@ clicks()
 // $('#next').click(function() {
 //   moveToSelected('next');
 // });
+document.addEventListener('touchstart', this.touchstart);
+document.addEventListener('touchmove', this.touchmove);
 
+function touchstart(e) {
+    e.preventDefault()
+}
+
+function touchmove(e) {
+    e.preventDefault()
+}
